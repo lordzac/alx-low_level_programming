2987b3020da38723632fcace30ea3159000d6b49
lordzac <balogunzacchaeus@gmail.com>
2022-09-11 10:59:45 -0700

    my sixth commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main- entry point for the program
+* main-entry point for the program
 * return:0 if no errors, return non zero if errors
 */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return(0);
+		printf("with proper grammer, but the outcome is a piece of art,\n");
+		return (0);
 }
