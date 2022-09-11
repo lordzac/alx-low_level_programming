8742dbcc594ebf2b1eb0d432f734f5cf2a55d43d
lordzac <balogunzacchaeus@gmail.com>
2022-09-10 22:46:28 -0700

    my fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
  * this program prints "programming is like building a multilingual puzzle
  * Return: 0
  * */
- int main(void)
+int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }		
