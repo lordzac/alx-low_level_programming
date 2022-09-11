de946ed1fa73667bbfeb93cffe9a449efb256872
lordzac <balogunzacchaeus@gmail.com>
2022-09-11 14:25:39 -0700

    my nineth commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - program entry point
  * Return: 1 on success
@@ -6,5 +6,5 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0)
+	return (1)
 }
