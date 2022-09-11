dbcc30b1d0b108c77a4d83d0870ec7d472028178
lordzac <balogunzacchaeus@gmail.com>
2022-09-10 23:43:39 -0700

    my fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
- *
- * Description: using the main function
- * this program prints programming is like building a multilingual puzzle
- * Returns nothing
- */
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints programming is like building a multilingual puzzle
+* Returns nothing
+*/
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"programming is like building a multilingual puzzle");
+	return(0);
 }		
