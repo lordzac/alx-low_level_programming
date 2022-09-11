964dabc81a8354a2e8aba8a1ca788fd3ccb8e034
lordzac <balogunzacchaeus@gmail.com>
2022-09-10 22:13:05 -0700

    my fifth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints programming is like building a multilingual puzzle
+ * Return: by default return zero if no errors otherwise return non-zero value
+ * */
+ int main(void)
+{
+
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}		
