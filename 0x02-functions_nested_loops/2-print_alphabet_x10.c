f7a3e693ac937647019059fefde0d464a86e5d61
lordzac <balogunzacchaeus@gmail.com>
2022-09-14 16:27:22 -0700

    my third commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet_x10 - print alphabet 10 times
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int result = 0;
+	char letter = 'a';
+	while (result < 10)
+	{
+		letter = 'a'
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+	_putchar('\n');
+	
+	result++;
+	}
+}
