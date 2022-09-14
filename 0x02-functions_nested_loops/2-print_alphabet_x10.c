6113fb181275d0ab43abd44380a246a31ffd240b
lordzac <balogunzacchaeus@gmail.com>
2022-09-14 16:37:55 -0700

    my third coomit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * print_alphabet_x10 - print alphabet 10 times
  * Return: Always 0
  */
@@ -11,7 +10,7 @@ void print_alphabet_x10(void)
 	char letter = 'a';
 	while (result < 10)
 	{
-		letter = 'a'
+		letter = 'a';
 		while (letter <= 'z')
 		{
 			_putchar(letter);
